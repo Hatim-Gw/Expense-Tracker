67386dd20ee21202b38db75d4feed23a505372c3
Hatim-Gw <m09181014@it.misuratau.edu.ly>
2026-03-24 08:59:20 +0200

    uI

---
+++ src/components/AddTransaction.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useState } from "react";
+const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+  return (
+    <div>
+      <h3>Add transaction</h3>
+      <form>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="enter text here..."
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">Amount</label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="enter amount here..."
+          />
+        </div>
+        <button className="btn">Add Transaction</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddTransaction;
---
+++ src/components/TransactionList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TransactionList = () => {
+  return (
+    <div>
+      <h3>History</h3>
+      <ul className="list">
+        <li className="minus">
+          Cash <span>-$4.00</span>
+          <button className="delete-btn">x</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default TransactionList;
